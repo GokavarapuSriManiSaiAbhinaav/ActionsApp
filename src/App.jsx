99d5d832b8fb6fe39d25c1b846ab11dca0af169a
ABHINAV <2300031133@gmail.com>
2025-10-15 20:40:07 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Welcome to My React Project</h1>
-      <h2>This is a Practice simple homepage deployed on GitHub Actions using Vite.</h2>
+      <h2>Simple homepage deployed on GitHub Actions using Vite.</h2>
     </div>
   );
 }
